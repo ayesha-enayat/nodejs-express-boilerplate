d26ae0e02744853cbbfa20429f182902763a3cdd
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2025-04-07 10:53:48 +0500

    add nodemailer

--- backend/src/modules/product/services/post.mjs
+++ backend/src/modules/product/services/post.mjs
@@ -1,8 +1,38 @@
 import { addData } from '../db/index.mjs';
+import nodemailer from 'nodemailer';
 
 
-const postData=async(data)=>{
-return await addData(data);
+//make this sendEmail Function in Helper
+const sendEmail = () => {
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: "your gmail ",
+            pass: " your app password",
+        },
+    });
+    const mailOptions = {
+        from: 'your email',
+        to: 'recipient email',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!',
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Email sent: ' + info.response);
+    });
+
+}
+
+
+const postData = async (data) => {
+    sendEmail();
+    return await addData(data);
 }
 
 export default postData;
